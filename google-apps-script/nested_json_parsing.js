ae111c8388e294f360d5923fcfe4388bdf973931
tehhowch <tehhowch@users.noreply.github.com>
2018-08-20 20:18:39 -0500

    GPL header

--- google-apps-script/nested_json_parsing.js
+++ google-apps-script/nested_json_parsing.js
@@ -1,3 +1,13 @@
+/** nested_json_parsing.js
+ * Copyright (c) 2018 by tehhowch
+ * This is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU General Public License as published by the Free Software
+ * Foundation, either version 3 of the License, or (at your option) any later version.
+ * This is distributed in the hope that it will be useful, but WITHOUT ANY
+ * WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A
+ * PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ */
+
 /**
  * These two functions allow rudimentary parsing of a JSON feed based on input desired field paths.
  * Written to parse example nested data, using Google Apps Script native methods.
